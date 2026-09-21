d62f25c4801e72575a24c7e03c87395a5c6f5f6e
d0vgan <dvv81@ukr.net>
2026-09-21 19:11:07 +0300

    updating Compile*.bat

--- Compile.bat
+++ Compile.bat
@@ -1,3 +1,4 @@
 @echo off
+echo Compiling...
 fbc -dll -gen gcc "Smart-Math.bas" "Smart-Math-Format.bas" "Smart-Math-CopyNormalize.bas" "Smart-Math-About.bas" "MathParser.bas" "ConfigManager.bas" "Res\Resource.rc" -x "SmartMath.dll"
 pause
